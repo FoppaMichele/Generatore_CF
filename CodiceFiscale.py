069b40f089f549f406acbb534e4cadfd2011c896
CarraraAlessandro <carrara.alessandro.09@itisdalmine.edu.it>
2026-02-26 21:02:29 +0100

    Aggiunta colonna c

--- CodiceFiscale.py
+++ CodiceFiscale.py
@@ -1,34 +1,98 @@
 import requests
-def trova_lettere_CF(stringa):
-    consonanti = "bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ"
-    CFname=''
-    count=0
-    for c in stringa:
-        if c in consonanti:
-            if count==1 or count==3 or count==4:
-                CFname+=c
-            count+=1
-    return CFname
+from datetime import datetime
+
+#COLONNA A -> Damian Alex
+
+#COLONNA B -> Foppa Michele
+
+#COLONNA C -> Carrara Alessandro
+
+def rimuoviLettereAccentate(Parola: str):
+    return Parola.lower().replace ("à", "a").replace("ò", "o").replace ("è", "e").replace("é", "e").replace ("ì", "i").replace ("ù", "u")
+
+def chiediComune ():
+    comune_valido = False
+    while not comune_valido:
+        comune = input ("Inserisci il comnune: ").strip().title()
+        com_dati = requests.get ("https://comuni-ita.nicolorebaioli.dev/comuni")
+        com_dati = com_dati.json()
+        trovato = False
+        for c in com_dati:
+            if c ["nome"] == comune:
+                trovato = True
+        if trovato:
+            return comune
+        else:
+            print ("Comune non trovato! riprovare")
+
+def calcolaCodiceMese (data: datetime):
+    cod_mese = None
+    mese = data.month
+    if mese == 1:
+        cod_mese = "A"
+    elif mese == 2:
+        cod_mese = "B"
+    elif mese == 3:
+        cod_mese = "C"
+    elif mese == 4:
+        cod_mese = "D"
+    elif mese == 5:
+        cod_mese = "E"
+    elif mese == 6:
+        cod_mese = "H"
+    elif mese == 7:
+        cod_mese = "L"
+    elif mese == 8:
+        cod_mese = "M"
+    elif mese == 9:
+        cod_mese = "P"
+    elif mese == 10:
+        cod_mese = "R"
+    elif mese == 11:
+        cod_mese = "S"
+    elif mese == 12:
+        cod_mese = "T"
+    return cod_mese
+
+def calcolaCodiceNome (nome: str):
+    cons = []
+    voc = []
+    cod_nome = ""
+    for let in nome:
+        if not (let == "a" or let == "e" or let == "i" or let == "o" or let == "u"):
+            cons.append (let)
+        else:
+            voc.append (let)
+    if len(cons) >= 4:
+        cod_nome += cons[0] + cons[2] + cons[3]
+    elif len (cons) == 3:
+        cod_nome += cons[0] + cons[1] + cons[2]
+    elif len (cons) == 2:
+        if len(voc) >= 1:
+            cod_nome += cons [0] + cons [1] + voc [0]
+        elif len (voc) == 0:
+            cod_nome += cons [0] + cons[1] + "x"
+    elif len (cons) == 1:
+        if len(voc) >= 2:
+            cod_nome += cons [0] + voc[0] + voc[1]
+        elif len(voc) == 1:
+            cod_nome += cons[0] + voc[0] + "x"
+        elif len (voc) == 0:
+            cod_nome += cons[0] + "xx"
+    elif len(cons) == 0:
+        if len (voc) >= 3:
+            cod_nome += voc [0] + voc[1] + voc[2]
+        elif len (voc) == 2:
+            cod_nome += voc[0] + voc[1] + "x"
+        elif len (voc) == 1:
+            cod_nome += voc[0] + "xx"
+        elif len(voc) == 0:
+            cod_nome += "xxx"
+    return cod_nome.upper()
+
+def rimuoviSpazi (Parola: str):
+    return Parola.replace (" ", "")
 
 
-def get_codice_catastale(comune):
-    url = "https://raw.githubusercontent.com/matteocontrini/comuni-json/master/comuni.json"
-    response = requests.get(url)
-    response.raise_for_status()
-    
-    comuni = response.json()
-    comune = comune.upper().strip()
-    
-    for comune in comuni:
-        if comune["nome"].upper() == comune:
-            return comune["codiceCatastale"]
-    
-    return None
-def get_dati_utente():
-    cf=''
-    nome=input("Inserisci il tuo nome:").upper()
-    cf+=trova_lettere_CF(nome)    
-    cognome=input("Inserisci il tuo cognome:").upper()
-    cf+=trova_lettere_CF(cognome)
-    print(cf)
-get_dati_utente()
+#MAIN
+#Da fare alla fine 
