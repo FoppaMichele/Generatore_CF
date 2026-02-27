b73f588fdfc5860dc5c8a4bfbf3c18257e93d02e
Foppa michele <foppamichele@MacBook-Pro-di-Foppa.local>
2026-02-27 15:15:13 +0100

    Rifatto la commit del calcolatore per il CIN

--- CodiceFiscale.py
+++ CodiceFiscale.py
@@ -55,14 +55,38 @@ def calcolaCodiceGiorno(data:datetime,sesso:str):
     else:
         giorno=str(data.day+40)
     return giorno
-def calcolaCodiceControllo():
-    nome=calcolaCodiceNome(chiediNome())
-    cognome=calcolaCodiceCognome(chiediCognome())
-    data=chiediDataNascita()
-    sesso=chiediSesso()
-    data_cf=f"{calcolaCodiceGiorno(data,sesso)}/{calcolaCodiceMese(data)}/{calcolaCodiceAnno(data)}"
-    codice_comune=calcolaCodiceComune(chiediComune())
-    print(data_cf)
+
+def calcoloCIN(cf:str):
+    dispari = {
+    '0': 1, '1': 0, '2': 5, '3': 7, '4': 9, '5': 13, '6': 15, '7': 17, '8': 19,
+    '9': 21, 'A': 1, 'B': 0, 'C': 5, 'D': 9, 'E': 13, 'F': 13, 'G': 15, 'H': 17,
+    'I': 19, 'J': 21, 'K': 2, 'L': 4, 'M': 18, 'N': 20, 'O': 11, 'P': 3, 'Q': 17,
+    'R': 8, 'S': 12, 'T': 14, 'U': 16, 'V': 10, 'W': 22, 'X': 25, 'Y': 24, 'Z': 23
+    }
+
+    pari = {
+    '0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8,
+    '9': 9, 'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7,
+    'I': 8, 'J': 9, 'K': 10, 'L': 11, 'M': 12, 'N': 13, 'O': 14, 'P': 15, 'Q': 16,
+    'R': 17, 'S': 18, 'T': 19, 'U': 20, 'V': 21, 'W': 22, 'X': 23, 'Y': 24, 'Z': 25
+    }
+
+    resto= {
+    0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'E', 5: 'F', 6: 'G',
+    7: 'H', 8: 'I', 9: 'J', 10: 'K', 11: 'L', 12: 'M', 13: 'N',
+    14: 'O', 15: 'P', 16: 'Q', 17: 'R', 18: 'S', 19: 'T', 20: 'U',
+    21: 'V', 22: 'W', 23: 'X', 24: 'Y', 25: 'Z'
+    }
+    tot=0
+    
+    for i in range(len(cf)):
+        if (i+1)%2==0:
+            tot+=pari[cf[i]]    
+        else:
+            tot+=dispari[cf[i]]             
+    Cin=tot%26
+    print(Cin)
+    return resto[Cin]
 
 #COLONNA C -> Carrara Alessandro
 
@@ -154,7 +178,3 @@ def rimuoviSpazi (Parola: str):
 
 #MAIN
 #Da fare alla fine 
-data=chiediDataNascita()
-sesso=chiediSesso()
-data_cf=f"{calcolaCodiceGiorno(data,sesso)}/{calcolaCodiceMese(data)}/{calcolaCodiceAnno(data)}"
-print(data_cf)
